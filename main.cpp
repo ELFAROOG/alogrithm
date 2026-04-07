dba03d8fbb0eb7074c4e37fa74a1adca3f815d32
ELFAROOG <faroog20043575@gmail.com>
2026-04-07 19:32:42 +0300

    Intial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip> // ·≈ŸÂ«— «·√—ﬁ«„ «·⁄‘—Ì… »œﬁ…
+
+using namespace std;
+
+int main() {
+    double a, b, Area;
+    const double PI = 3.141592653589793;
+
+    cout << "Enter value for a: ";
+    cin >> a;
+    cout << "Enter value for b: ";
+    cin >> b;
+
+
+    if ((2 * a + b) == 0) {
+        cout << "Error: Division by zero!" << endl;
+    } else {
+
+        Area = PI * (a * a / 4.0) * ((2 * a - b) / (2 * a + b));
+
+        cout << fixed << setprecision(2);
+        cout << "Circle Area: " << Area << endl;
+        cout << "Floor of Area: " << floor(Area) << endl;
+    }
+
+    return 0;
+}
